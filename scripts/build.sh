f702d9bea35383b44b4d2f03e29226dbce6c695b
apotocki <sane22222@aim.com>
2024-06-21 16:00:06 +0200

    update versions

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 THREAD_COUNT=$(sysctl hw.ncpu | awk '{print $2}')
 HOST_ARC=$( uname -m )
 XCODE_ROOT=$( xcode-select -print-path )
-OPENSSL_VER=openssl-3.2.2
+OPENSSL_VER=openssl-3.3.1
 #MACOSX_VERSION_ARM=12.3
 #MACOSX_VERSION_X86_64=10.13
 ################## SETUP END
